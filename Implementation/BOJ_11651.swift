0196a28c1939227cf470f2cbf8cbd4918a0aa41d
kth1210 <k2hop1210@gmail.com>
2023-10-27 18:33:15 +0900

    [SOLVE] : BOJ_11651 좌표 정렬하기 2

---
+++ Implementation/BOJ_11651.swift
@@ -0,0 +1,25 @@
+//
+//  BOJ_11651.swift
+//  Algorithm_Swift
+//
+//  Created by 김태현 on 2023/10/27.
+//
+
+typealias coordinate = (x: Int, y: Int)
+
+let N = Int(readLine()!)!
+
+var coordinates: [coordinate] = []
+for _ in 0..<N {
+    let input = readLine()!
+        .split(separator: " ")
+        .map { Int(String($0))! }
+    
+    coordinates.append((input[0], input[1]))
+}
+
+coordinates
+    .sorted(by: {
+        $0.y == $1.y ? $0.x < $1.x : $0.y < $1.y
+    })
+    .forEach { print("\($0.x) \($0.y)") }
